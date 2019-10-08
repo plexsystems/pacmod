22b295315a87d17419ae64885118fc2da5d81e8a
John Reese <john@reese.dev>
2019-10-08 08:41:38 -0400

    Remove extra newline

--- internal/commands/default.go
+++ internal/commands/default.go
@@ -4,7 +4,6 @@ import "github.com/spf13/cobra"
 
 // NewDefaultCommand creates a new default command
 func NewDefaultCommand() *cobra.Command {
-
 	cmd := cobra.Command{
 		Use:   "pacmod <subcommand>",
 		Short: "Command line tool to assist with packaging Go modules",
